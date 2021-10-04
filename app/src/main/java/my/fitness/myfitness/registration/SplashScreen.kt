3da4c88abbb7c6d1fa6650e08dc9b0644a3d689a
AmerBiro <amer.biro@gmail.com>
2021-10-04 22:45:59 +0200

    adding delay to splash screen

--- app/src/main/java/my/fitness/myfitness/registration/SplashScreen.kt
+++ app/src/main/java/my/fitness/myfitness/registration/SplashScreen.kt
@@ -1,17 +1,16 @@
 package my.fitness.myfitness.registration
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
 import android.os.Handler
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
 import my.fitness.myfitness.R
 import my.fitness.myfitness.databinding.RegistrationSplashScreenBinding
 
@@ -20,6 +19,10 @@ class RegistrationSplashScreen : Fragment() {
 
     private var _binding: RegistrationSplashScreenBinding? = null
     private val binding get() = _binding!!
+    private var user = FirebaseAuth.getInstance().currentUser
+    private var delay = Handler()
+    private var duration : Int?= null
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -37,19 +40,41 @@ class RegistrationSplashScreen : Fragment() {
 
     override fun onStart() {
         super.onStart()
-        handler()
+        autoSignIn()
     }
 
 
-    fun handler() {
+    fun autoSignIn() {
+        var accountStatus = binding.accountStatus
+        accountStatus.text = "Checking account..."
+
         var x = Runnable {
-            findNavController().navigate(R.id.registration)
+            findNavController().navigate(R.id.action_registrationSplashScreen_to_registration)
         }
 
-        var y = Handler()
-        y.postDelayed(x, 3000)
-    }
+        var y = Runnable {
+            findNavController().navigate(R.id.action_registrationSplashScreen_to_home2)
+        }
 
+        var z = Runnable {
+            if (user == null) {
+                accountStatus.text = "No account founded!"
+                duration = (100..1000).random()
+                delay.postDelayed(x, duration!!.toLong())
+                Log.d(TAG, "autoSignIn: $duration")
+            } else {
+                accountStatus.text = "Logging in..."
+                duration = (250..1250).random()
+                delay.postDelayed(y, duration!!.toLong())
+                Log.d(TAG, "autoSignIn: $duration")
+            }
+        }
+        duration = (1500..2000).random()
+        delay.postDelayed(z, duration!!.toLong())
+        Log.d(TAG, "autoSignIn: $duration")
+
+
+    }
 
 
 }
