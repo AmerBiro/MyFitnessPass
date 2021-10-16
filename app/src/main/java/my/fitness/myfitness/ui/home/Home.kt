fb8505e70e10e47f4afa58940b5ca66b4f9584ec
Sulai <s195462@student.dtu.dk>
2021-10-16 16:06:37 +0200

    added bottomnavigation

--- app/src/main/java/my/fitness/myfitness/ui/home/Home.kt
+++ app/src/main/java/my/fitness/myfitness/ui/home/Home.kt
@@ -5,18 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import my.fitness.myfitness.R
-
+import my.fitness.myfitness.ui.home.programs.*
 
 
 class Home : Fragment() {
 
 
-//    private val programs = Program()
-//    private val fitnesss = Fitness()
-//    private val graphs = Graph()
-//    private val menuFragments = MenuFragment()
-//    private val profiles = Profile()
+    private val programs = ProgramView()
+    private val fitnesss = FitnessCoachView()
+    private val graphs = GraphView()
+    private val menuFragments = MenuView()
+    private val profiles = ProgramView()
 
 
 
@@ -26,35 +27,35 @@ class Home : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
 
-//        val view =  inflater.inflate(R.layout.home, container, false)
-//
-//        replaceFragment(programs)
-//
-//        val bottom = view.findViewById<BottomNavigationView>(R.id.bottom_navigation)
-//
-//
-//        bottom.setOnNavigationItemSelectedListener {
-//            when(it.itemId){
-//                R.id.program -> replaceFragment(programs)
-//                R.id.menu -> replaceFragment(menuFragments)
-//                R.id.graph -> replaceFragment(graphs)
-//                R.id.fitness -> replaceFragment(fitnesss)
-//                R.id.profile -> replaceFragment(profiles)
-//            }
-//            true
-//        }
-//
-//        bottom.selectedItemId =  R.id.program
-//
-//
-//
-//
-//        /*
-//        BottomNavigationView bottomNavigationView;
-//        bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavigationView);
-//        bottomNavigationView.setOnNavigationItemSelectedListener(myNavigationItemListener);
-//        bottomNavigationView.setSelectedItemId(R.id.my_menu_item_id);
-//        * */
+        val view =  inflater.inflate(R.layout.home, container, false)
+
+        replaceFragment(programs)
+
+        val bottom = view.findViewById<BottomNavigationView>(R.id.bottom_navigation)
+
+
+        bottom.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.program -> replaceFragment(programs)
+                R.id.menu -> replaceFragment(menuFragments)
+                R.id.graph -> replaceFragment(graphs)
+                R.id.fitness -> replaceFragment(fitnesss)
+                R.id.profile -> replaceFragment(profiles)
+            }
+            true
+        }
+
+        bottom.selectedItemId =  R.id.program
+
+
+
+
+        /*
+        BottomNavigationView bottomNavigationView;
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(myNavigationItemListener);
+        bottomNavigationView.setSelectedItemId(R.id.my_menu_item_id);
+        * */
 
         return view
     }
