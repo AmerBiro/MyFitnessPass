755ccc35310144f89e7d2d9a8422b5d28eadc89f
AmerBiro <amer.biro@gmail.com>
2021-10-21 14:31:11 +0200

    adding all necessary files to the project

---
+++ app/src/main/java/myfitnesspass/MyFitnessPassApplication.kt
@@ -0,0 +1,14 @@
+package myfitnesspass
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
+
+@HiltAndroidApp
+class MyFitnessPassApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(Timber.DebugTree())
+    }
+}
---
+++ app/src/main/java/myfitnesspass/data/local/database/ProgramDatabase.kt
@@ -0,0 +1,18 @@
+package myfitnesspass.data.local.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import myfitnesspass.data.local.dao.ProgramDao
+import myfitnesspass.data.local.entities.Program
+import myfitnesspass.data.local.others.Converters
+
+@Database(
+    entities = [Program::class],
+    version = 1
+)
+@TypeConverters(Converters::class)
+abstract class ProgramDatabase : RoomDatabase() {
+
+    abstract fun programDao(): ProgramDao
+}
