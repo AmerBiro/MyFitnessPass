f76a926bcf298882a453726ac36e218d303b5408
AmerBiro <amer.biro@gmail.com>
2021-10-22 21:24:19 +0200

    adding logout functionality

--- app/src/main/java/myfitnesspass/other/Constants.kt
+++ app/src/main/java/myfitnesspass/other/Constants.kt
@@ -7,6 +7,9 @@ object Constants {
     const val KEY_LOGGED_IN_EMAIL = "KEY_LOGGED_IN_EMAIL"
     const val KEY_LOGGED_IN_PASSWORD = "KEY_LOGGED_IN_PASSWORD"
 
+    const val NO_KEY_LOGGED_IN_EMAIL = "NO_KEY_LOGGED_IN_EMAIL"
+    const val NO_KEY_LOGGED_IN_PASSWORD = "NO_KEY_LOGGED_IN_PASSWORD"
+
     const val ENCRYPTED_SHARED_PREF_NAME = "enc_shared_pref"
 
     const val BASE_URL = "http://192.168.43.130:8001"
--- app/src/main/java/myfitnesspass/ui/profile/fragments/ProfileFragment.kt
+++ app/src/main/java/myfitnesspass/ui/profile/fragments/ProfileFragment.kt
@@ -1,20 +1,68 @@
 package myfitnesspass.ui.profile.fragments
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavOptions
+import androidx.navigation.fragment.findNavController
+import dagger.hilt.android.AndroidEntryPoint
 import myfitnesspass.fitness.myfitness.R
+import myfitnesspass.fitness.myfitness.databinding.ProfilePhotoBinding
+import myfitnesspass.fitness.myfitness.databinding.ProfileViewBinding
+import myfitnesspass.other.Constants.KEY_LOGGED_IN_EMAIL
+import myfitnesspass.other.Constants.KEY_LOGGED_IN_PASSWORD
+import myfitnesspass.other.Constants.NO_KEY_LOGGED_IN_EMAIL
+import myfitnesspass.other.Constants.NO_KEY_LOGGED_IN_PASSWORD
 import myfitnesspass.ui.BaseFragment
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class ProfileFragment : BaseFragment(R.layout.profile_view){
 
+    @Inject
+    lateinit var sharedPreferences: SharedPreferences
+
+    private var _binding: ProfileViewBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.profile_view, container, false)
+        _binding = ProfileViewBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.lodOut.setOnClickListener{
+            logout()
+        }
+    }
+
+
+    private fun logout(){
+        sharedPreferences.edit().putString(KEY_LOGGED_IN_EMAIL, NO_KEY_LOGGED_IN_EMAIL).apply()
+        sharedPreferences.edit().putString(KEY_LOGGED_IN_PASSWORD, NO_KEY_LOGGED_IN_PASSWORD).apply()
+//        val navOptions =  NavOptions.Builder()
+//            .setPopUpTo(R.id.homeView, true)
+//            .build()
+//        findNavController().navigate(
+//            R.id.loginFragment, navOptions
+//        )
+        findNavController().navigate(R.id.action_homeView_to_loginFragment)
+    }
+
+
+
+
+
+
+
+
 }
