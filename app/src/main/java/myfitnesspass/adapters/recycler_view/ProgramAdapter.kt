c148953ff3904817ac01389430ae11229264e8c2
Sulai <s195462@student.dtu.dk>
2021-10-29 00:08:32 +0200

    added menu item to ProgramAdapter.kt

--- app/src/main/java/myfitnesspass/adapters/recycler_view/ProgramAdapter.kt
+++ app/src/main/java/myfitnesspass/adapters/recycler_view/ProgramAdapter.kt
@@ -1,8 +1,12 @@
 package myfitnesspass.adapters.recycler_view
 
+import android.content.Context
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.PopupMenu
+import androidx.appcompat.view.menu.MenuView
 import androidx.recyclerview.widget.AsyncListDiffer
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
@@ -10,8 +14,8 @@ import myfitnesspass.data.local.entities.Program
 import myfitnesspass.fitness.myfitness.R
 import kotlinx.android.synthetic.main.program_list_view.view.*
 
-
-class ProgramAdapter : RecyclerView.Adapter<ProgramAdapter.ProgramViewHolder>() {
+//https://www.youtube.com/watch?v=k_UZkS_L61Y kotlin android how to add in recyclerview popup menu
+class ProgramAdapter(val c:Context) : RecyclerView.Adapter<ProgramAdapter.ProgramViewHolder>() {
 
     inner class ProgramViewHolder(itemView: View): RecyclerView.ViewHolder(itemView)
 
@@ -33,30 +37,82 @@ class ProgramAdapter : RecyclerView.Adapter<ProgramAdapter.ProgramViewHolder>() 
         set(value) = differ.submitList(value)
 
 
+    //test
+    private var programName = arrayOf("Crossfit Madmen","Peaks and Valleys")
+    private var coachName = arrayOf("By Thomas Andersen","By Sulaiman Kasas")
+    private var programDescription = arrayOf("This course has been designed\n for fat burning and muscles gowing","This course has been designed\n for fat burning and muscles gowing")
+    private var programdays = arrayOf("3 days","2 days")
+    private var programdate = arrayOf("12/10/2021 12/11/2021","12/01/2022 12/02/2022")
+
+
+
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProgramViewHolder {
-        return ProgramViewHolder(
-            LayoutInflater.from(parent.context).inflate(
+        val v = LayoutInflater.from(parent.context).inflate(
                 R.layout.program_list_view,
                 parent,
                 false
             )
-        )
+        return ProgramViewHolder(v)
     }
 
     override fun getItemCount(): Int {
-        return programs.size
+       // return programs.size
+        return programName.size
     }
 
     override fun onBindViewHolder(holder: ProgramViewHolder, position: Int) {
-        val program = programs[position]
+       // val program = programs[position]
         holder.itemView.apply {
-            program_name.text = program.programName
-            program_coach.text = program.coach
-            program_description.text = program.description
-            program_days.text = program.days.toString()
+//            program_name.text = program.programName
+//            program_coach.text = program.coach
+//            program_description.text = program.description
+//            program_days.text = program.days.toString()
+////            program_exercises.text = program
+//            program_date.text = "$program.startDate.toString() - ${program.endDate.toString()}"
+
+            program_name.text = programName[position]
+            program_coach.text = coachName[position]
+            program_description.text = programDescription[position]
+            program_days.text = programdays[position]
 //            program_exercises.text = program
-            program_date.text = "$program.startDate.toString() - ${program.endDate.toString()}"
+            program_date.text = programdate[position]
+
+
+
+            menu_buttons.setOnClickListener {
+                popupMenu(it)
+
+            }
         }
+    }
+
+
+    private fun popupMenu(v:View){
+        val popupMenus = PopupMenu(c.applicationContext,v)
+        popupMenus.inflate(R.menu.program_item_more)
+        popupMenus.setOnMenuItemClickListener {
+            when(it.itemId){
+                R.id.edit -> {
+                    true
+                }
+
+                R.id.delete ->{
+                    true
+                }
+
+                else -> true
+
+            }
+        }
+
+        popupMenus.show()
+        popupMenus.gravity = Gravity.LEFT
+        val popup = PopupMenu::class.java.getDeclaredField("mPopup")
+        popup.isAccessible = true
+        val menu = popup.get(popupMenus)
+        menu.javaClass.getDeclaredMethod("setForceShowIcon", Boolean::class.java)
+            .invoke(menu,true)
     }
 
 
--- app/src/main/java/myfitnesspass/ui/home/programs/fragments/ShowProgramsFragment.kt
+++ app/src/main/java/myfitnesspass/ui/home/programs/fragments/ShowProgramsFragment.kt
@@ -5,6 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import myfitnesspass.adapters.recycler_view.ProgramAdapter
 import myfitnesspass.fitness.myfitness.R
 import myfitnesspass.fitness.myfitness.databinding.FragmentProgramShowBinding
 import myfitnesspass.ui.BaseFragment
@@ -14,6 +17,9 @@ class ShowProgramsFragment: BaseFragment(R.layout.fragment_program_show) {
     private var _binding: FragmentProgramShowBinding? = null
     private val binding get() = _binding!!
 
+    private var layoutManger: RecyclerView.LayoutManager? = null
+    private var adapter: RecyclerView.Adapter<ProgramAdapter.ProgramViewHolder>?  =null
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -21,6 +27,13 @@ class ShowProgramsFragment: BaseFragment(R.layout.fragment_program_show) {
     ): View? {
         _binding = FragmentProgramShowBinding.inflate(inflater, container, false)
         val view = binding.root
+
+        layoutManger = LinearLayoutManager(requireContext())
+
+        binding.recyclerview.layoutManager = layoutManger
+
+        adapter  =ProgramAdapter(requireContext())
+        binding.recyclerview.adapter = adapter
 
 
         binding.addButton.setOnClickListener {
