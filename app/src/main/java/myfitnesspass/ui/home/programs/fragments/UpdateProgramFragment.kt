50aff0bbce7ae146a52f071c0a93ba0965d245b8
Sulai <s195462@student.dtu.dk>
2021-11-18 18:12:39 +0100

    add AlertDialog to UpdateProgramFragment.kt

--- app/src/main/java/myfitnesspass/ui/home/programs/fragments/UpdateProgramFragment.kt
+++ app/src/main/java/myfitnesspass/ui/home/programs/fragments/UpdateProgramFragment.kt
@@ -1,10 +1,13 @@
 package myfitnesspass.ui.home.programs.fragments
 
+import android.app.AlertDialog
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import androidx.navigation.fragment.findNavController
 import myfitnesspass.fitness.myfitness.R
 import myfitnesspass.fitness.myfitness.databinding.FragmentProgramCreationIBinding
@@ -31,9 +34,9 @@ class UpdateProgramFragment : BaseFragment(R.layout.fragment_update_program) {
         }
 
 
-        binding.linearEditProgramName.setOnClickListener {
-            findNavController().navigate(R.id.action_updateProgramFragment_to_programCreationI)
-        }
+//        binding.linearEditProgramName.setOnClickListener {
+//            findNavController().navigate(R.id.action_updateProgramFragment_to_programCreationI)
+//        }
 
         binding.buttonDateProgram.setOnClickListener {
             findNavController().navigate(R.id.action_updateProgramFragment_to_programCreationII)
@@ -47,9 +50,53 @@ class UpdateProgramFragment : BaseFragment(R.layout.fragment_update_program) {
             findNavController().navigate(R.id.action_updateProgramFragment_to_programCreationIIIII)
         }
 
-
-
+        showEditTextName()
+        showEditTextDescription()
         return view
+    }
+
+
+    private fun showEditTextName(){
+        binding.linearEditProgramName.setOnClickListener {
+            val builder = AlertDialog.Builder(requireContext())
+            val inflater = requireActivity().layoutInflater
+            val dialogLayout = inflater.inflate(R.layout.edit_text_layout,null)
+            val editText = dialogLayout.findViewById<EditText>(R.id.edit_text_bt)
+
+            with(builder){
+                setTitle("Enter program name!")
+                setPositiveButton("Ok"){dialog,which ->
+                    binding.textViewName.text= editText.text.toString()
+                }
+                setNegativeButton("Cancel"){dialog,which ->
+                    Log.d("Main","Negative button clicked")
+                }
+                setView(dialogLayout)
+                show()
+            }
+        }
+    }
+
+
+    private fun showEditTextDescription(){
+        binding.linearEditProgramDescription.setOnClickListener {
+            val builder = AlertDialog.Builder(requireContext())
+            val inflater = requireActivity().layoutInflater
+            val dialogLayout = inflater.inflate(R.layout.edit_text_layout,null)
+            val editText = dialogLayout.findViewById<EditText>(R.id.edit_text_bt)
+
+            with(builder){
+                setTitle("Enter a new Description!")
+                setPositiveButton("Ok"){dialog,which ->
+                    binding.textViewDec.text= editText.text.toString()
+                }
+                setNegativeButton("Cancel"){dialog,which ->
+                    Log.d("Main","Negative button clicked")
+                }
+                setView(dialogLayout)
+                show()
+            }
+        }
     }
 
 }
